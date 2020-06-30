51622a9b11e5642aee51d3603be527f00f946e27
lorellana672 <lorellana672@famaf.unc.edu.ar>
2020-06-30 20:11:43 -0300

    Something to start on

---
+++ app/src/main/java/com/orellana/poket/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.orellana.poket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    //private val normalType:Int = R.id.normal
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    normal.setOnclickListener(){
+        Toast.makeText(this,"hola",Toast.LENGTH_LONG)
+    }
+
+   /* override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.normal ->
+        }
+    }*/
+
+
+}
